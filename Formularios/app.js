1c1d5105fac8f96325bc7b0effd1a8f2947dc835
Brarias <braianrodrigo14@gmail.com>
2022-01-24 23:04:16 -0300

    finalizando practica de formularios

--- Formularios/app.js
+++ Formularios/app.js
@@ -12,8 +12,24 @@ const regExpObjeto = new RegExp("Braian", "i"); // notacion de objeto
 console.log(regExp.test("mlktji dfgbhbbn bb"));*/
 
 const formulario = document.getElementById("formulario");
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
 
+const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regUserEmail =
+  /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("formulario procesado");
+
+  console.log("userName: " + userName.value);
+  console.log("userEmail: " + userEmail.value);
+
+  if (
+    !(regUserName.test(userName.value) && regUserEmail.test(userEmail.value))
+  ) {
+    console.log("Formato no válido");
+    return;
+  }
+
+  console.log("formulario enviado");
 });
